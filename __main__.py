3252de8ce233278ed615e23e75e198b4700275be
sourabhdey <sourabhdey21@gmail.com>
2024-09-10 13:15:36 +0000

    create vpc

--- __main__.py
+++ __main__.py
@@ -9,4 +9,8 @@ bucket = s3.Bucket('pulumi-bucket')
 # Export the name of the bucket
 pulumi.export('bucket_name', bucket.id)
 
+# Create an AWS resource (VPC)
+vpc = s3.Vpc('pulumi-vpc', cidr_block='10.0.0.0/16')
+pulumi.export('vpc_id', vpc.id)
+
 
