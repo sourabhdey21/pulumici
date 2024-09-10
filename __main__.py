132ae9814ec12c0e287aabdc2baa81749fb51978
sourabhdey <sourabhdey21@gmail.com>
2024-09-10 13:26:43 +0000

    added the subnet config also

--- __main__.py
+++ __main__.py
@@ -1,27 +1,29 @@
 """An AWS Python Pulumi program"""
 
 import pulumi
-from pulumi_aws import s3
-import pulumi_aws as aws
-
+from pulumi_aws import s3, ec2
 
 # Create an AWS resource (S3 Bucket)
 bucket = s3.Bucket('jarvis-bucket')
 
 # Create a new VPC
-vpc = aws.ec2.Vpc("my-vpc",
+vpc = ec2.Vpc("my-vpc",
     cidr_block="10.1.0.0/16",
     tags={
-        "Name": "jarvis-vpc",
+        "tags": {
+            "Name": "jarvis-vpc",
+        },
     })
 
-# Create an internet gateway
-igw = aws.ec2.InternetGateway("my-igw",
+# Create a subnet
+subnet = ec2.Subnet("my-subnet",
     vpc_id=vpc.id,
+    cidr_block="10.1.1.0/24",
+    availability_zone="us-east-1a",
     tags={
-        "tags": [
-            {"key": "Name", "value": "jarvis-igw"},
-        ],
+        "tags": {
+            "Name": "jarvis-subnet",
+        },
     })
 
 # Export the VPC ID
